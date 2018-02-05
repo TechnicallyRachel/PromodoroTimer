536b7eca73c40ad5046b1a4803db79c98c4ef243
Rachel Giles <paintmetothemoon@gmail.com>
2018-02-05 15:04:36 -0800

    Added "Press enter to start:"

--- PomodoTimer.py
+++ PomodoTimer.py
@@ -3,9 +3,15 @@ import datetime
 
 #Function for 25 minute timer with countdown
 def promodoro():
-    print('Ready, set, BE PRODUCTIVE!')
+    print('Welcome to the Promodoro Timer!\n'
+          'This timer is set for 25 minute intervals,'
+          '\nwith 5 minute breaks!\nAfter 4 25 minute intervals and breaks,' 
+          'you get 15 minutes for a snack!')
+    print raw_input("Press enter to start:")
     while True:
+        #25 minute Timer with 5 minute breaks repeated 4 times
         for _ in range(4): #x4 repeat loop
+            print('Ok, ready to be productive? GO!')
             #1500 seconds = 25 minutes
             for t in range(1500,-1,-1):
                 #Countdown timer
@@ -16,7 +22,7 @@ def promodoro():
                 print(timeformat)
                 time.sleep(1)
                 if t == 0:
-                    print('Ding!')
+                    print('Ding! Nice job. Go take a 5 minute break!')
             #Time stamp for referencing how long you've been going.
             print(datetime.datetime.now())
             #300 = 5 min
